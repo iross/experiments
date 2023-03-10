fa928fa659ae39bb30ad2dcc8a365d5ebc43a3f0
iaross <iaross@cosmos0001.chtc.wisc.edu>
2023-03-10 09:54:17 -0600

    xdd changes

--- semantic-search/search.py
+++ semantic-search/search.py
@@ -17,22 +17,38 @@ import torch
 from transformers import logging
 from sentence_transformers import SentenceTransformer
 import pandas as pd
+import glob
+import requests
 
 
 # silence transformers logging. by default, mpnet prints out some unnecessary warnings
 logging.set_verbosity_error()
 
 
+def get_title(docid):
+    resp = requests.get(f"https://xdd.wisc.edu/api/articles?docid={docid}")
+    return resp.json()['success']['data'][0]['title']
+
 def get_data(size=400) -> pd.DataFrame:
     #read in data, and remove rows with missing abstracts
-    df = pd.read_csv('data/papers.csv')
-    df = df[['title', 'abstract', 'paper_text']]
-    df = df[df['abstract'] != 'Abstract Missing']
+    input_files = glob.glob("data/*.txt")
+    data = []
+    with open("doclist", "w") as fout:
+        fout.write("\n".join(input_files))
+    for f in input_files:
+        docid = f.replace("data/", "").replace(".txt", "")
+        text = open(f).read()[:5000]
+        title = get_title(docid)
+        data.append({"docid" : docid, "title" : title, "paper_text": text, "abstract": text})
+
+#    df = pd.read_csv('data/papers.csv')
+    df = pd.DataFrame(data)
+#    df = df[df['abstract'] != 'Abstract Missing']
     df.reset_index(inplace=True, drop=True)
 
     #take a random subset of the data since it all probably won't fit in memory
-    df = df.sample(size, random_state=42)
-    df.reset_index(inplace=True, drop=True)
+#    df = df.sample(size, random_state=42)
+#    df.reset_index(inplace=True, drop=True)
 
     return df
 
@@ -40,8 +56,9 @@ def get_data(size=400) -> pd.DataFrame:
 def get_title_abstract_data(size=400) -> list[str]:
     """Create a list of strings consisting of the title and abstract of each paper"""
     df = get_data(size)
-    data = (df['title'] + '\n' + df['abstract']).tolist()
-    return data
+#    data = (df['title'] + '\n' + df['abstract']).tolist()
+    data = (df['title'] + '\n' + df['paper_text']).tolist()
+    return data, df['docid'].tolist()
 
 
 
@@ -84,7 +101,7 @@ class MPNetEmbedder:
 
     def embed(self, texts:list[str]):
         with torch.no_grad():
-            embeddings = self.model.encode(texts, show_progress_bar=False, device=self.device, convert_to_tensor=True)
+            embeddings = self.model.encode(texts, show_progress_bar=True, device=self.device, convert_to_tensor=True)
             return embeddings
 
 
@@ -92,14 +109,18 @@ class MPNetEmbedder:
 
 def main():
     from easyrepl import REPL
+    import pickle
 
-    # instantiate the embedding model. 
+    # instantiate the embedding model.
     # Note that Specter performs much worse than MPNet on this task, and probably shouldn't be used
     embedder = MPNetEmbedder()
 
     #embed the corpus of data. Elements are the title+abstract of each paper
-    corpus = get_title_abstract_data()
+    corpus, docids = get_title_abstract_data()
     corpus_embeddings = embedder.embed(corpus)
+    pickle.dump(corpus_embeddings, open("output/embeddings.pkl", "wb"))
+    pickle.dump(docids, open("output/docids.pkl", "wb"))
+    pickle.dump(corpus, open("output/corpus.pkl", "wb"))
 
 
     #number of results to display
@@ -111,7 +132,7 @@ def main():
 
         # compute similarity scores
         scores = torch.nn.functional.cosine_similarity(query_embedding, corpus_embeddings, dim=1)
-        
+
         #get the indices of the top_k scores
         top_results = torch.topk(scores, k=top_k).indices.tolist()
 
@@ -119,11 +140,11 @@ def main():
         for idx in top_results:
             print()
             print(f"Match Score: {scores[idx]}")
-            print(f"Text: {corpus[idx]}")
+            print(f"docid: {docids[idx]}, Text: {corpus[idx]}")
             print()
 
         print('-------------------------------------------------')
-        
+
 
 
 if __name__ == '__main__':
